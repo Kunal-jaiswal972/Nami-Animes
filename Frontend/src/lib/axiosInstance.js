4ef3886c0fe661d2c42ac03fc8ce5efbf1edd98d
kunal <kishujaiswal3@gmail.com>
2023-10-12 21:01:47 +0530

    Please NO!!!!

--- Frontend/src/lib/axiosInstance.js
+++ Frontend/src/lib/axiosInstance.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const env = import.meta.env.VITE_REACT_APP_ENVIRONMENT;
-const backendUrl = import.meta.env.VITE_REACT_APP_BACKEND_URL;
+const backendUrl = import.meta.env.VITE_REACT_APP_BACKEND_URL + "/api/v1";
 
 const instance = axios.create({
-  baseURL:
-    env !== "development" ? backendUrl : "http://localhost:8080" + "/api/v1",
+  baseURL: env !== "development" ? backendUrl : "http://localhost:8080/api/v1",
   // You can also add other configuration options here, such as headers, timeout, etc.
 });
 
